15312eed06f2ec88be5807844cead0d1219277d9
Woo-Ung <137769190+Woo-Ung@users.noreply.github.com>
2026-08-28 23:51:48 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 1.32 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int[] arr, int[,] queries) 
+    {
+        int[] answer = new int[arr.Length];              
+        
+        for(int i = 0; i < queries.GetLength(0); i++)
+        {            
+            for(int j = queries[i,0]; j <= queries[i,1];j++)
+            {                
+                if(j % queries[i,2] == 0)
+                {                    
+                    arr[j]++;                   
+                }                
+            }    
+        }
+        return answer = arr;
+    }
+}
