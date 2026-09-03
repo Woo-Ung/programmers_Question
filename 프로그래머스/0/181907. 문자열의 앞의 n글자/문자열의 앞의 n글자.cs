903580d7372af92ab50e86caaf8c72ff3608b038
Woo-Ung <137769190+Woo-Ung@users.noreply.github.com>
2026-09-03 09:07:10 +0900

    [level 0] Title: 문자열의 앞의 n글자, Time: 0.56 ms, Memory: 47.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181907. 문자열의 앞의 n글자/문자열의 앞의 n글자.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int n) {
+        string answer = "";
+        
+        for(int i = 0; i< n;i++)
+        {
+            answer += my_string[i];
+        }
+        
+        return answer;
+    }
+}
