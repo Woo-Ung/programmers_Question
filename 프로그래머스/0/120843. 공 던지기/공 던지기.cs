5b402c4c6a4d4bd19593c7cfd18d341994815a9c
Woo-Ung <137769190+Woo-Ung@users.noreply.github.com>
2026-09-05 00:11:40 +0900

    [level 0] Title: 공 던지기, Time: 0.10 ms, Memory: 46.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        
+        answer = numbers[(k-1)*2 %numbers.Length];      
+        
+        return answer;
+    }
+}
