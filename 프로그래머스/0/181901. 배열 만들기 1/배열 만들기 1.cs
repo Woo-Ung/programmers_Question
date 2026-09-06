cb77f1a4583012f19f3b7db387fc40d9fa42a706
Woo-Ung <137769190+Woo-Ung@users.noreply.github.com>
2026-09-06 23:52:11 +0900

    [level 0] Title: 배열 만들기 1, Time: 1.89 ms, Memory: 46.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Solution 
+{
+    public int[] solution(int n, int k) 
+    {
+        int[] answer = new int[n/k];
+        int count=0;
+        
+        for(int i = 1; i<=n;i++)
+        {
+            if(i%k == 0)
+            {
+                answer[count++] = i; 
+            }    
+        }
+        
+        return answer;
+    }
+}
