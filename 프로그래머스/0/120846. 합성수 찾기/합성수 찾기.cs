f051e433dfa5b204bf54d3a7996a1fa55a55e183
Woo-Ung <137769190+Woo-Ung@users.noreply.github.com>
2026-09-13 23:21:53 +0900

    [level 0] Title: 합성수 찾기, Time: 0.10 ms, Memory: 45.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120846. 합성수 찾기/합성수 찾기.cs
@@ -0,0 +1,23 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        int answer = 0;
+        
+        for(int i = 2; i<=n;i++)
+        {
+            for(int j = 2; j<i;j++)
+            {
+                if (i%j == 0)
+                {
+                    answer++;
+                    break;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
